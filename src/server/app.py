421c1449acf69e9b551722e2ff09ab44c935555f
mgsweet <mgsweet@126.com>
2020-05-02 16:52:36 +1000

    REST API now load data from db

--- src/server/app.py
+++ src/server/app.py
@@ -9,12 +9,22 @@ CORS(app, resources=r'/*')
 @app.route('/api/statistic', methods=['GET'])
 def get_statistic_api():
     # test: http://0.0.0.0:3000/api/statistic?city=melbourne
-    city = request.args.get('city').lower()
-    return city_info_loader.load_city_info(city)
+    # test: http://0.0.0.0:3000/api/statistic?city=melbourne&suburb=AIRPORT%20WEST
+    city = request.args.get('city')
+    suburb = request.args.get('suburb')
+    res = {}
+    if (city != None):
+        if (suburb != None):
+            res = city_info_loader.load_suburb_info(city, suburb)
+        else:
+            res = city_info_loader.load_city_info(city)
+    return res
 
 if __name__ == '__main__':
-    # Use for debug
-    # with app.test_request_context('/api/statistic?city=melbourne'):
-    #     city = request.args.get('city')
-        # statistic_data = analysis_loader.load_city_analysis(city)
     app.run(debug=False, port=3000, host='0.0.0.0')
+    # Use for debug
+    # with app.test_request_context('/api/statistic?city=melbourne&suburb=AIRPORT%20WEST'):
+        # city = request.args.get('city')
+        # suburb = request.args.get('suburb')
+        # print(city, suburb)
+        # statistic_data = analysis_loader.load_city_analysis(city)
--- src/server/utils/city_info_loader.py
+++ src/server/utils/city_info_loader.py
@@ -1,34 +1,53 @@
 import json
-from backend import analysis_loader
+import os
+from couchDB import db_util
+from configparser import ConfigParser
 
-def _load_geo(city):
-    data_path = "suburbs/{}_suburbs.json".format(city)
-    with open(data_path, 'r') as f:
-        data = json.load(f)
-        return data
+# http://172.26.130.149:5984/_utils/
+# admin:admin1234
 
-def _load_analysis(city):
-    data = analysis_loader.load_city_analysis(city)
-    return data
+def _couchdb_get_url(section='DEFAULT', verbose=False):
+    config = ConfigParser()
+    url_file = 'config/server.url.cfg'
+    if verbose:
+        print('url_file {}'.format(url_file))
+    config.read(url_file)
+    server_url = config.get(section, 'server_url')
+    return server_url
+
+def _load_from_db(db_name, key):
+    server_url = _couchdb_get_url()
+    try:
+        db = db_util.cdb(server_url, db_name)
+        return db.getByKey(key)
+    except Exception as e:
+        return {}
+        # raise e
+
+
+def _load_city_analysis_from_db(city):
+    return _load_from_db("analysis_results", "{}_analysis_result".format(city))
+
+def _load_city_geoinfo_from_db(city):
+    return _load_from_db("aurin", "{}_suburbs".format(city))
 
 def _is_city_valid(city):
-    # Current available city: "Melbourne", "Sydney", "Brisbane", "Adelaide", "Perth(WA)"
+    # Current available city: "Melbourne", "Sydney", "Brisbane", "Adelaide", "Perth"
     valid_city = {"melbourne", "sydney", "brisbane", "adelaide", "perth"}
     return city in valid_city
 
 def _get_merge_data(city):
-    geo_data = _load_geo(city)
-    analysis_data = _load_analysis(city)
-    for suburb in geo_data['features']:
+    data = _load_city_geoinfo_from_db(city)
+    analysis_data = _load_city_analysis_from_db(city)
+    for suburb in data['features']:
         try:
             suburb_name = suburb['properties']['name']
             suburb_analysis = analysis_data['suburbs'][suburb_name]
             suburb['properties']['analysis_result'] = suburb_analysis
         except Exception as e:
             # Ignore but print the error
-            print('Error from utils/city_info_loader.py: ',e)
-    return geo_data
-            
+            print('Error from utils/city_info_loader.py: ', e)
+    return data
 
 def load_city_info(city):
     """
@@ -37,7 +56,19 @@ def load_city_info(city):
     :return dict City information
     """
     res = {}
+    city = city.lower()
     if _is_city_valid(city):
         res = _get_merge_data(city)
     return res
+
+def load_suburb_info(city, suburb):
+    city = city.lower()
+    suburb = suburb.lower()
+    city_info = load_city_info(city)
+    for suburb in city_info['features']:
+        if suburb['properties']['name'] == suburb:
+            return suburb
+    return {}
+
+
 
