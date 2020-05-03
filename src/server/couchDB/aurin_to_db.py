6046f46c5ee976bf023c8d0633fff333b696cf11
ShanRB <rashan@student.unimelb.edu.au>
2020-05-03 17:54:42 +1000

    remove parentheses

--- src/server/couchDB/aurin_to_db.py
+++ src/server/couchDB/aurin_to_db.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 import db_util
 
 COUCHDB_URL = 'http://{}:{}@{}:{}/'
@@ -19,7 +20,7 @@ for filename in os.listdir(dirpath):
     data = json.load(content)
     _id = filename[:-5]
     if filename.endswith('_suburbs.json'):
-        db.put(data,_id)
+        #db.put(data,_id)
         pass
     else:
         data_toload = {}
@@ -32,7 +33,8 @@ for filename in os.listdir(dirpath):
             suburbdata = item['properties']
             del suburbdata['sa2_name16']
             for suburbname in suburbnamelist:
-                suburbname = suburbname.strip()
+                suburbname = re.sub(r" ?\([^)]+\)", "", suburbname)
+                suburbname = suburbname.strip().upper()
                 data_toload['suburbs'][suburbname] = suburbdata
                 #print(f'{suburb_oriname} ---> {suburbname}')
         db.put(data_toload,_id)
