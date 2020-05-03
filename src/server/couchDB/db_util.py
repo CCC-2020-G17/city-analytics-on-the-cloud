ba341614efcb284b99fec03d632d7b6cc4253dbb
ShanRB <rashan@student.unimelb.edu.au>
2020-05-03 17:54:09 +1000

    integer block input enabled

--- src/server/couchDB/db_util.py
+++ src/server/couchDB/db_util.py
@@ -143,11 +143,11 @@ class cdb:
 
         # define start and end key for search
         if cityname is None:
-            startkey = [start_ts]
-            endkey = [end_ts,{}]
+            startkey = [str(start_ts)]
+            endkey = [str(end_ts),{}]
         else:
-            startkey = [start_ts,cityname]
-            endkey = [end_ts,cityname]
+            startkey = [str(start_ts),cityname]
+            endkey = [str(end_ts),cityname]
         # whether to include doc or just get id
         if only_id:
             for item in self.db.view('cities/get_timestamp',\
