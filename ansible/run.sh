3ee0512cc7f8100ebcc1d3ab9747db8b732dc628
mgsweet <mgsweet@126.com>
2020-05-14 20:12:49 +1000

    couchdb cluster trial

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-. ./openrc.sh; ansible-playbook -i playbooks/inventory/inventory.ini --ask-become-pass --tags "servers-init" site.yaml
+. ./openrc.sh; ansible-playbook -i playbooks/inventory/inventory.ini --ask-become-pass --tags "test" site.yaml
