e94e27dd1c4c8ea5e9ef8f962b29bbeaf9c1ba1d
ShanRB <rashan@student.unimelb.edu.au>
2020-05-16 19:22:05 +1000

    error for no connection

--- src/server/couchDB/initial_deployment/init_deploy.py
+++ src/server/couchDB/initial_deployment/init_deploy.py
@@ -19,77 +19,84 @@ username = parser.parse_args().username
 password = parser.parse_args().password
 
 url = 'http://{}:{}@{}:5984/'.format(username,password,serverURL)
-
+connected = False
 couchserver = couchdb.Server(url)
-print(f'connected to {url}')
+try:
+    print(f'connected to {couchserver}: {couchserver.version()}')
+    connected = True
+except ConnectionRefusedError:
+    print(f'Connection refused by {url}')
 
 dbs = ['analysis_results','aurin','tweets_for_test','tweets_mixed','tweets_with_geo','twitters']
 
-# create databases
-for db in dbs:
-    try:
-        couchserver.create(db)
-        print(f'database created: <{db}>')
-    except Exception:
-        print(f'database <{db}> already exists')
-    
-
-# save map_reduce views
-dir_path = os.path.dirname(os.path.realpath(__file__))
-mapreduce_dir = os.path.join(dir_path,'design_cities.json')
-mapreduce_doc = json.load(open(mapreduce_dir,'r'))
-map_dbs = ['tweets_for_test','tweets_mixed','tweets_with_geo','twitters']
-for db in map_dbs:
-    couchdb = couchserver[db]
-    try:
-        if '_rev' in mapreduce_doc:
-            del mapreduce_doc['_rev']
+if connected:
+    # create databases
+    for db in dbs:
+        try:
+            couchserver.create(db)
+            print(f'database created: <{db}>')
+        except ConnectionRefusedError:
+            print('Connection refused')
+        except Exception:
+            print(f'database <{db}> already exists')
         
-        couchdb.save(mapreduce_doc)
-        print(f'design/cities view is created in <Database {db}>')
-    except Exception:
-        _rev = couchdb['_design/cities']["_rev"]
-        mapreduce_doc["_rev"] = _rev
-        couchdb.save(mapreduce_doc)
-        print(f'design/cities view is updated in <Database {db}>')
 
-# save analysis results
-analysis_files = ['adelaide_analysis_result.json','brisbane_analysis_result.json',\
-    'melbourne_analysis_result.json','perth_analysis_result.json','sydney_analysis_result.json']
-couchdb = couchserver['analysis_results']
-for filename in analysis_files:
-    analysis_file = open(os.path.join(dir_path,filename),'r')
-    data = json.load(analysis_file)
-    analysis_file.close()
-    if '_rev' in data:
-        del data['_rev']
-    try:
-        couchdb.save(data)
-        print(f'save {filename} to {couchdb}')
-    except Exception:
-        key = data['_id']
-        _rev = couchdb[key]["_rev"]
-        data["_rev"] = _rev
-        couchdb.save(data)
-        print(f'udpate {filename} to <DATABASE {couchdb}')
+    # save map_reduce views
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    mapreduce_dir = os.path.join(dir_path,'design_cities.json')
+    mapreduce_doc = json.load(open(mapreduce_dir,'r'))
+    map_dbs = ['tweets_for_test','tweets_mixed','tweets_with_geo','twitters']
+    for db in map_dbs:
+        couchdb = couchserver[db]
+        try:
+            if '_rev' in mapreduce_doc:
+                del mapreduce_doc['_rev']
+            
+            couchdb.save(mapreduce_doc)
+            print(f'design/cities view is created in <Database {db}>')
+        except Exception:
+            _rev = couchdb['_design/cities']["_rev"]
+            mapreduce_doc["_rev"] = _rev
+            couchdb.save(mapreduce_doc)
+            print(f'design/cities view is updated in <Database {db}>')
 
-# save aurin data
-couchdb = couchserver['aurin']
-aurin_dir = os.path.join(dir_path,'aurin/')
-for filename in os.listdir(aurin_dir):
-    aurinfilename = os.path.join(aurin_dir,filename)
-    datafile = open(aurinfilename,'r')
-    data = json.load(datafile)
-    datafile.close()
+    # save analysis results
+    analysis_files = ['adelaide_analysis_result.json','brisbane_analysis_result.json',\
+        'melbourne_analysis_result.json','perth_analysis_result.json','sydney_analysis_result.json']
+    couchdb = couchserver['analysis_results']
+    for filename in analysis_files:
+        analysis_file = open(os.path.join(dir_path,filename),'r')
+        data = json.load(analysis_file)
+        analysis_file.close()
+        if '_rev' in data:
+            del data['_rev']
+        try:
+            couchdb.save(data)
+            print(f'save {filename} to {couchdb}')
+        except Exception:
+            key = data['_id']
+            _rev = couchdb[key]["_rev"]
+            data["_rev"] = _rev
+            couchdb.save(data)
+            print(f'udpate {filename} to <DATABASE {couchdb}')
 
-    if '_rev' in data:
-        del data['_rev']
-    try:
-        couchdb.save(data)
-        print(f'save {filename} to <DATABASE {couchdb}')
-    except Exception:
-        key = data['_id']
-        _rev = couchdb[key]["_rev"]
-        data["_rev"] = _rev
-        couchdb.save(data)
-        print(f'udpate {filename} to {couchdb}')
+    # save aurin data
+    couchdb = couchserver['aurin']
+    aurin_dir = os.path.join(dir_path,'aurin/')
+    for filename in os.listdir(aurin_dir):
+        aurinfilename = os.path.join(aurin_dir,filename)
+        datafile = open(aurinfilename,'r')
+        data = json.load(datafile)
+        datafile.close()
+
+        if '_rev' in data:
+            del data['_rev']
+        try:
+            couchdb.save(data)
+            print(f'save {filename} to <DATABASE {couchdb}')
+        except Exception:
+            key = data['_id']
+            _rev = couchdb[key]["_rev"]
+            data["_rev"] = _rev
+            couchdb.save(data)
+            print(f'udpate {filename} to {couchdb}')
