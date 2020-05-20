2c313ad32b79498cfe556ce94ce90b0750e6c729
mgsweet <mgsweet@126.com>
2020-05-20 16:58:03 +1000

    update current ip for easy check

--- ansible/checkCouchDB.sh
+++ ansible/checkCouchDB.sh
@@ -1,4 +1,4 @@
-curl http://cccg17:cccg17@172.26.132.161:5984/_cluster_setup
-curl http://cccg17:cccg17@172.26.132.197:5984/_cluster_setup
-curl http://cccg17:cccg17@172.26.133.61:5984/_cluster_setup
-curl http://cccg17:cccg17@172.26.132.161:5984/_membership
+curl http://cccg17:cccg17@172.26.130.126:5984/_cluster_setup
+curl http://cccg17:cccg17@172.26.133.140:5984/_cluster_setup
+curl http://cccg17:cccg17@172.26.133.214:5984/_cluster_setup
+curl http://cccg17:cccg17@172.26.130.126:5984/_membership
